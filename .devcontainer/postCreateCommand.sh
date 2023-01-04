7f7756c19f735ae722b660659b6b7a0772d81669
Steven Clontz <steven.clontz@gmail.com>
2023-01-04 15:14:35 +0000

    update devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env bash
-
 echo "Install LaTeX"
-apt-get update && apt-get install -y texlive texlive-latex-extra texlive-fonts-extra texlive-xetex texlive-science --no-install-recommends
+apt update && apt install -y texlive texlive-latex-extra texlive-fonts-extra texlive-xetex texlive-science --no-install-recommends
 
 echo "Install sage"
-apt-get install -y sagemath --no-install-recommends
+apt install -y sagemath --no-install-recommends
 
 echo "Install PDF tools"
-apt install ghostscript pdf2svg
+apt install -y ghostscript pdf2svg --no-install-recommends
